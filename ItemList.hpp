dcd98aacc9ddeb030f45be36053f722e89189f8f
Michael <michaelleoni356@gmail.com>
2023-11-11 00:10:19 -0700

    Made Items External

--- ItemList.hpp
+++ ItemList.hpp
@@ -4,5 +4,5 @@
 #include "RangedWeapon.h"
 
 
-MeleeWeapon dagger = MeleeWeapon("dagger","A small knife, potentially good for stabbing","finesse","1d4",1.0);
-RangedWeapon longbow = RangedWeapon("longbow","A bow, longer than average.","Two Handed","1d8",2.0,150,600);
+extern MeleeWeapon dagger = MeleeWeapon("dagger","A small knife, potentially good for stabbing","finesse","1d4",1.0);
+extern RangedWeapon longbow = RangedWeapon("longbow","A bow, longer than average.","Two Handed","1d8",2.0,150,600);
